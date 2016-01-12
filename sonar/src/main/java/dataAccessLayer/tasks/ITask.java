fb7583e85b3bcd193b4ee1a3838ca6b17868f0f7
Slaan <de.SlannesH@gmail.com>
2016-01-12 13:04:32 +0100

    Extended ITask Interface

--- sonar/src/main/java/dataAccessLayer/tasks/ITask.java
+++ sonar/src/main/java/dataAccessLayer/tasks/ITask.java
@@ -1,7 +1,15 @@
 package dataAccessLayer.tasks;
 
+import java.util.List;
+
 /**
  * Created by Daniel Hofmeister on 12.01.2016.
  */
 public interface ITask {
+
+  public Operation getType();
+
+  public List<TeamformationInput> getInput();
+
+  public List<TeamformationOutput> getOutput();
 }
